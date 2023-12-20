9035d08e652a336c612090dee407b17399964abe
sleepyNajlio <najliolootfi@gmail.com>
2023-12-20 16:42:03 +0100

    initial commit

---
+++ src/dogs/dto/create-dog.dto.ts
@@ -0,0 +1,10 @@
+import { IsString, IsInt } from 'class-validator';
+
+export class CreateDogDto {
+    @IsString()
+    name: string;
+    @IsInt()
+    age: number;
+    @IsString()
+    breed: string;
+}
