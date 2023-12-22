425b2c25d58a4d3ad246de74fd86a7581bd8d32d
sleepyNajlio <najliolootfi@gmail.com>
2023-12-22 16:40:02 +0100

    removed auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, 
+  imports: [ 
     UsersModule, 
     PrismaModule,
     ConfigModule.forRoot({isGlobal: true})],
